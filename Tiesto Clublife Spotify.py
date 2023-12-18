49aa02dea456520f67ceebd87ae3413fa2960b37
Ramsey Kilani <kilani.ramsey@gmail.com>
2023-12-18 03:18:35 -0500

    Added dynamic setlist URL

--- Tiesto Clublife Spotify.py
+++ Tiesto Clublife Spotify.py
@@ -1,3 +1,4 @@
+##### Imports #########
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -7,14 +8,9 @@ import time
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import configparser
+from datetime import datetime, timedelta
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-client_id = config['spotify']['client_id']
-client_secret = config['spotify']['client_secret']
-episode = "870"
-
+##### Function ##########
 
 def save_html_with_selenium(url, file_name):
     # Set up the Selenium WebDriver
@@ -22,7 +18,7 @@ def save_html_with_selenium(url, file_name):
     driver = webdriver.Chrome(service=service)
 
     driver.get(url)
-    time.sleep(5)  # Wait for the page to load completely
+    time.sleep(3)  # Wait for the page to load completely
 
     # Save the page source to a file
     with open(file_name, 'w', encoding='utf-8') as file:
@@ -30,11 +26,6 @@ def save_html_with_selenium(url, file_name):
     
     print(f"HTML content saved to {file_name}")
     driver.quit()
-
-# Example usage
-url = 'https://www.1001tracklists.com/tracklist/2sxdnp5t/tiesto-tiestos-club-life-870-2023-12-02.html'
-save_html_with_selenium(url, f'C:\\Users\\kilan\\IDrive-Sync\\Documents\\Coding\\tiesto-spotify-playlist\\tiesto_club_life_{episode}.html')
-
 
 def get_songs_from_set(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -53,13 +44,21 @@ def get_songs_from_set(file_path):
     
     return songs
 
-# Example usage
-file_path = f'C:\\Users\\kilan\\IDrive-Sync\\Documents\\Coding\\tiesto-spotify-playlist\\tiesto_club_life_{episode}.html'  # Replace with the local path to your HTML file
-songs_list = get_songs_from_set(file_path)
-for song in songs_list:
-    print(song)
+def get_episode_url(episode_number):
+    with open('C:\\Users\\kilan\\IDrive-Sync\\Documents\\Coding\\tiesto-spotify-playlist\\HTMLs\\index.html', 'r', encoding='utf-8') as file:
+        soup = BeautifulSoup(file, 'html.parser')
 
-def create_spotify_playlist(songs, playlist_name):
+    # Search for the episode link
+    episode_link = None
+    links = soup.find_all('a')
+    for link in links:
+        if link.text.strip() == f"Tiësto - Club Life {episode_number}":
+            episode_link = link.get('href')
+            break
+
+    return episode_link
+
+def create_spotify_playlist(songs, playlist_name,client_id,client_secret):
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                    client_secret=client_secret,
                                                    redirect_uri="http://localhost:8888/callback",
@@ -77,4 +76,34 @@ def create_spotify_playlist(songs, playlist_name):
 
     sp.playlist_add_items(playlist['id'], track_ids)
 
-create_spotify_playlist(songs_list, f'Tiesto Club Life {episode}')
+####### Code ###########
+
+config = configparser.ConfigParser()
+config.read('C:\\Users\\kilan\\IDrive-Sync\\Documents\\Coding\\tiesto-spotify-playlist\\config.ini')
+
+client_id = config['spotify']['client_id']
+client_secret = config['spotify']['client_secret']
+episode_number = "867"
+episode_html_path = f'C:\\Users\\kilan\\IDrive-Sync\\Documents\\Coding\\tiesto-spotify-playlist\\HTMLs\\tiesto_club_life_{episode_number}.html'
+
+# Save index file
+index_url = 'https://www.1001tracklists.com/source/6wndmv/tiestos-club-life/index.html'
+index_path = 'C:\\Users\\kilan\\IDrive-Sync\\Documents\\Coding\\tiesto-spotify-playlist\\HTMLs\\index.html'
+save_html_with_selenium(index_url, index_path)
+
+# Get current episode url
+episode_url = get_episode_url(int(episode_number))
+print(episode_url)
+
+# Get episode song list
+if episode_url:
+    full_url = 'https://www.1001tracklists.com' + episode_url  # Assuming the fetched URL is relative
+    # save_html_with_selenium(full_url, episode_html_path)
+    songs_list = get_songs_from_set(episode_html_path)
+    for song in songs_list:
+        print(song)
+else:
+    print(f"Episode {episode_number} not found.")
+
+# Create playlist from list
+create_spotify_playlist(songs_list, f'Tiesto Club Life {episode_number}',client_id,client_secret)
